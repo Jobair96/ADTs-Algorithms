72ed184b2fc2397effcf307b022f91a10784f187
Jobair Hassan <jobair.hassan96@gmail.com>
2014-09-21 18:11:53 -0400

    Adds the implementation of a hash table.
    The hash table has been implemented in C++. It using templates heavily,
    so it may seem cryptic. It has a few subtle bugs which are planned to be
    fixed.

---
+++ ADTs/Hash Table/HashTable.h
@@ -0,0 +1,36 @@
+//#ifndef HASHTABLE_H
+//#define HASHTABLE_H
+//
+///*
+//	This file implements a HashTable using C++ vectors. It uses the open hashing with chaining method.
+//	*/
+//
+//#include <vector>
+//#include <string>
+//#include "Node.h"
+//
+//template<typename T>
+//class HashTable
+//{
+//private:
+//	std::vector<Node<T>> *table;
+//	int hash(int key) const;
+//
+//public:
+//	HashTable(int size);
+//	void insert(std::string name, int snum);
+//	bool lookup(int snum) const;
+//	void remove(int snum);
+//	void print() const; // For debugging
+//	virtual ~HashTable();
+//
+//
+//};
+//
+//template<typename T>
+//HashTable<T>::void insert(T data)
+//{
+//
+//}
+//
+//#endif
