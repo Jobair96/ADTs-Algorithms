cf2d6d5d5c54587b9ce43ad94c052130a1c83e0c
Jobair Hassan <jobair.hassan96@gmail.com>
2014-12-11 20:55:41 -0500

    Updates .gitignore to ignore temprary vim files.

--- ADTs/Linked List/Node.h
+++ ADTs/Linked List/Node.h
@@ -1,13 +1,16 @@
 /**
 
 This file contains the definition of the node structure that will be utilized in the linked list, stack, queue, and BST data structures.
+
 */
 
 #include <stdio.h>
 
 
-struct Node
+typedef struct
 {
+	// Pointer to the next node in the list.
 	struct Node* next;
+
 	int data;
-};
+} Node;
