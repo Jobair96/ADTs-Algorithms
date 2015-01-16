360969ec66e4678097c24d43ec8288f3d5f00df8
Jobair Hassan <jobair.hassan@outlook.com>
2015-01-15 21:36:31 -0500

    Adds a test class for unit testing, and adds unit tests for the isEmpty() method.

---
+++ ADTs/Queue/QueueADT/QueueTest/QueueClassTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Data_Structures;
+
+namespace QueueTest {
+
+    [TestClass]
+    public class QueueClassTest {
+        [TestMethod]
+        public void TestMethodisEmpty() {
+
+            Queue<String> queue = new Queue<String>();
+
+            Assert.IsFalse(queue.isEmpty());
+        }
+    }
+}
